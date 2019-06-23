e5476873c90c288ce469ca7b72cdbb67ce592ddc
Sam <samkitchen94@gmail.com>
2019-06-23 13:59:16 +0100

    user can post, partially working but image not displaying on homepage. Again spiked

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def front
+    @posts = Post.order(created_at: :desc)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:new, :create]
   devise_for :users
   root to: 'home#front'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
